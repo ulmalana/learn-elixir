3753c10ad849ca31561e03ab72e1465ec07720fc
ulmalana <muhamaul@gmail.com>
2022-04-11 14:18:24 +0700

    [Elixir] started chapter 4

---
+++ ch4/todo_multi_dict.ex
@@ -0,0 +1,13 @@
+defmodule TodoList do
+   # import MultiDict
+
+    def new(), do: MultiDict.new()
+
+    def add_entry(todo_list, date, title) do
+        MultiDict.add(todo_list, date, title)
+    end
+
+    def entries(todo_list, date) do
+        MultiDict.get(todo_list, date)
+    end
+end
