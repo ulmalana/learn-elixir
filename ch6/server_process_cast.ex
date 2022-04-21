39238cbab1bf5fbc5733b1913d141432daefca5f
ulmalana <muhamaul@gmail.com>
2022-04-21 20:19:33 +0700

    [Elixir] finished chapter 6

--- ch6/server_process_cast.ex
+++ ch6/server_process_cast.ex
@@ -9,14 +9,21 @@ defmodule ServerProcess do
     defp loop(callback_module, current_state) do
         receive do
             {:call, request, caller} ->
-                {:cast, request} ->
-                    new_state =
-                        callback_module.handle_cast(
-                            request, 
-                            current_state
-                        )
-                    #send(caller, {:response, response})
-                    loop(callback_module, new_state)
+                {response, new_state} =
+                    callback_module.handle_call(
+                        request,
+                        current_state
+                    )
+                send(caller, {:response, response})
+                loop(callback_module, new_state)   
+
+             {:cast, request} ->
+                new_state =
+                    callback_module.handle_cast(
+                        request, 
+                        current_state
+                    )
+                loop(callback_module, new_state)
         end
     end
     
